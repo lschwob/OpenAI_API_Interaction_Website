a453022e6b0187814404881dd594205bbb6705be
Leonard <lowehardmail@gmail.com>
2023-03-16 20:16:13 +0100

    Correction bouton navbar pour téléphone

--- resources/views/ias.blade.php
+++ resources/views/ias.blade.php
@@ -7,8 +7,8 @@
     </h1>
     <div class="grid grid-cols-4 row">
         @foreach ($ias as $ia)
-          <div class="rounded-2xl mr-8 bg-gradient-to-r from-blue-500 via-blue-800 to-purple-900 p-1 shadow-xl shadow-blue-900 hover:transition-transform hover:-translate-y-2 hover:translate-x-2">
-            <a class="block rounded-xl bg-black p-4 sm:p-6 lg:p-8" href="">
+          <div class="rounded-2xl mr-8 mt-16 bg-gradient-to-r from-blue-500 via-blue-800 to-purple-900 p-1 shadow-xl shadow-blue-900 hover:transition-transform hover:-translate-y-2 hover:translate-x-2">
+            <a class="block rounded-xl bg-black p-4 sm:p-6 lg:p-8" href="{{ $ia->link }}">
               <div class="mt-16">
                 <h3 class="text-lg font-bold text-white sm:text-xl">
                   {{  $ia->title  }}
