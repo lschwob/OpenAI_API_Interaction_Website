02ec3d4c9e2158547b8dfc8e8fff8baddc3785e4
Leonard <lowehardmail@gmail.com>
2023-03-16 22:41:50 +0100

    lien home sur le logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
   <nav class="bg-white border-gray-200 px-4 lg:px-6 py-3.5 dark:bg-gray-800 border-b-2 border-opacity-80">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-        <a href="" class="flex items-center">
+        <a href="{{ route('home') }}" class="flex items-center">
             <img src="{{ asset('askwiselogo.png') }}" class="mr-3 h-6 sm:h-9" alt="Askwise Logo" />
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">AskWise</span>
         </a>
