fcd56fc28d9a62f9c5dbe7fe26c564a98daad6c0
Leonard <lowehardmail@gmail.com>
2023-03-16 22:40:04 +0100

    .

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <div class="flex flex-col items-center justify-center h-screen text-white">
         
-        <div class="custom-shape-divider-top-1678739135 mt-16">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <div class="custom-shape-divider-top-1678739135 mt-16 -z-10">
+            {{-- <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                 <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="fill-gray-900"></path>
-            </svg>
+            </svg> --}}
         </div>
         <h1 class="text-4xl text-center border-b-2 p-8">IAS Writer</h1>
     </div>
