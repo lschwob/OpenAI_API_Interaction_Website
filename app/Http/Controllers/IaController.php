13ac69b27b92b135f4948c82d9813259b8535563
Leonard <leo@askwise.online>
2023-03-28 21:09:50 +0000

    iacontroller

--- app/Http/Controllers/IaController.php
+++ app/Http/Controllers/IaController.php
@@ -307,7 +307,7 @@ class IaController extends Controller
             $path = $request->file('pdf')->store('temp');
             // Récupérer le chemin d'accès complet du fichier stocké
             $path = storage_path('app/' . $path);
-            dd($path);
+            // dd($path);
             $config = new \Smalot\PdfParser\Config();
             $config->setHorizontalOffset('');
 
